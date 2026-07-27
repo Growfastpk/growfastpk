5604df1badc8202f580e1bc59a3ccf2940670136
Growfastpk <bilalspall9115602@gmail.com>
2026-07-27 20:32:51 +0500

    Initialize Supabase client in config.js

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
 const SUPABASE_URL = "https://imfvbwozjcybmhulbikw.supabase.co/rest/v1/";
 
 const SUPABASE_ANON_KEY = "sb_publishable_M2XHAsmInlYyFlC9CWiYyw_KgBD47fl";
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
