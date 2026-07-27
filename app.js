5471c9f08caa610bc308f17fa6cc407a7e779dc1
Growfastpk <bilalspall9115602@gmail.com>
2026-07-27 21:44:24 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   console.log("GrowFastPK App Loaded");
-});
-document.getElementById("registerBtn")?.addEventListener("click", async () => {
 
-  const name = document.getElementById("fullName").value;
-  const email = document.getElementById("email").value;
-  const whatsapp = document.getElementById("whatsapp").value;
-  const password = document.getElementById("password").value;
+  const registerBtn = document.getElementById("registerBtn");
 
-  const { data, error } = await supabase.auth.signUp({
-    email: email,
-    password: password
-  });
+  if(registerBtn){
 
-  if(error){
-    document.getElementById("message").innerText = error.message;
-  } else {
-    document.getElementById("message").innerText = "Account created successfully";
+    registerBtn.addEventListener("click", async () => {
+
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+
+      const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: password
+      });
+
+      if(error){
+        document.getElementById("message").innerText = error.message;
+      } else {
+        document.getElementById("message").innerText = "Account created successfully";
+      }
+
+    });
+
   }
 
 });
