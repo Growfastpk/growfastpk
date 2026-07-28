a1e0cc33fc862e2f52da853992d67a493050c81b
Growfastpk <bilalspall9115602@gmail.com>
2026-07-28 20:42:05 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://imfvbwozjcybmhulbikw.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://imfvbwozjcybmhulbikw.supabase.co";
 
 const SUPABASE_ANON_KEY = "sb_publishable_M2XHAsmInlYyFlC9CWiYyw_KgBD47fl";
 const supabase = window.supabase.createClient(
